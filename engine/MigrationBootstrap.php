c5fc26befd5421824cdb5193c4db7449ea4bd25e
sharutyunyan7777@gmail.com <96731429+seryozhaharutyunyan@users.noreply.github.com>
2023-02-22 19:39:01 +0300

    1.0

---
+++ engine/MigrationBootstrap.php
@@ -0,0 +1,10 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+const ROOT_DIR=__DIR__;
+const DS=DIRECTORY_SEPARATOR;
+const ENV='App';
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/Function.php';
--- migration_rollbeck.php
+++ migration_rollbeck.php
@@ -1,13 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
-const ROOT_DIR=__DIR__;
-const DS=DIRECTORY_SEPARATOR;
-const ENV='App';
-
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/engine/Function.php';
+require_once __DIR__.'/engine/MigrationBootstrap.php';
 
 use \Engine\Core\Database\Connection;
 use Engine\Core\Migration\Migrations;
